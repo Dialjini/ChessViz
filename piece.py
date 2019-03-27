0ebf764ae33a78c37da59034c3ebd8003a04eeea
Dialjini <danil_kustov@mail.ru>
2019-03-27 21:29:28 +0300

    redo lab1 on python

---
+++ piece.py
@@ -0,0 +1,4 @@
+whites = [{'color': 'white', 'piece': 'king', 'canrock': '1'},
+          {'color': 'white', 'piece': 'queen'}]
+blacks = [{'color': 'black', 'piece': 'king', 'canrock': '1'},
+          {'color': 'white', 'piece': 'queen'}]
