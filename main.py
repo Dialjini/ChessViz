97e181a6e91767f7e08b7d335ab9029eea7f4786
Dialjini <danil_kustov@mail.ru>
2019-03-27 22:07:24 +0300

    add help command

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import piece
 import desk
 
+a = ''
+
 for i in desk.map:
     print(i)
+
+while (a != 'quit'):
+    a = input()
+    if (a == 'help'):
+        print('Available commands:')
+        print('Cord commands. For example a7-a5. (a-h, 1-8)')
+        print('back : Makes a step back in current game')
+        print('restart : restarts current game')
+
+    if (a == 'restart'):
+        desk.map = desk.default
+        for i in desk.map:
+            print(i)
