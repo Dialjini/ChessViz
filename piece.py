44805e1d79c4baf0e6d71331d2efc30f90865107
Dialjini <danil_kustov@mail.ru>
2019-03-28 11:18:51 +0300

    delete piece list, add pawn rules

--- piece.py
+++ piece.py
@@ -1,8 +1,22 @@
-piece = [{'color': 'white', 'name': 'k', 'canrock': '1', 'cord': ''},
-         {'color': 'white', 'name': 'q', 'cord': ''}]
+import main
 
 
 def getPiece(cord):
-    for i in piece:
-        if (i['cord'] == cord):
-            return i['name']
+    return main.a[cord[0]][cord[1]]
+
+
+def is_real(piece, cord1, cord2):
+    if (getPiece(cord1) == ' '):
+        return False
+
+    if (getPiece(cord1) == 'p'):
+        if (((cord1[2] - cord2[2]) != '1') | (cord1[2] - cord2[2] != '-1')):
+            if (((cord1[1] - cord2[1]) > '2') | (((cord1[1] - cord2) < '1'))):
+                return False
+            if (getPiece(cord2) != ' '):  # Pawn rules
+                return False
+        else:
+            if (getPiece(cord2) == ' '):
+                return False
+            else:
+                return True
