0494cf43b67097007a5088c34e8864dce4e37d2b
Dialjini <danil_kustov@mail.ru>
2019-03-28 10:21:28 +0300

    decomment main

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def reCord(cord):
 def getCord2(a):
     return reCord(cord=(a[3] + a[4]))
 
+
 def getCord1(a):
     return reCord(cord=(a[0] + a[1]))
 
@@ -20,23 +21,24 @@ def getCord1(a):
 for i in desk.map:
     print(i)
 
-# while (a != 'quit'):
-#   a = input()
-#  if (a == 'help'):
-#     print('Available commands:')
-#    print('Cord commands. For example a7-a5. (a-h, 1-8)')
-#   print('back : Makes a step back in current game')
-#  print('restart : restarts current game')
+while (a != 'quit'):
+    a = input()
+if (a == 'help'):
+    print('Available commands:')
+    print('Cord commands. For example a7-a5. (a-h, 1-8)')
+    print('back : Makes a step back in current game')
+    print('restart : restarts current game')
 
-# if (a == 'restart'):
-#  desk.map = desk.default
-#  for i in desk.map:
-#      print(i)
-# if (len(a) == 5):
-#    if (a[2] == '-'):
-#        cord1 = getCord1(a)
-#       cord2 = getCord2(a)
-#      if (desk.is_real(cord1=cord1, cord2=cord2)):
-#         desk.history.append({'step': a, 'stash': piece.getPiece(cord=cord2)})
-#    print('debug')
+if (a == 'restart'):
+    desk.map = desk.default
+    for i in desk.map:
+        print(i)
+if (len(a) == 5):
+    if (a[2] == '-'):
+        cord1 = getCord1(a)
+        cord2 = getCord2(a)
+        if (desk.is_real(cord1=cord1, cord2=cord2)):
+            desk.history.append({'step': a, 'stash': piece.getPiece(cord=cord2)})
+            print('debug')
+
 print(getCord2(a='a1-a2'))
