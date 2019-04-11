fbb7360aeecc835666ac45e051c6814e4a9a4c20
Dialjini <danil_kustov@mail.ru>
2019-04-11 15:07:14 +0300

    Add King rules

--- piece.py
+++ piece.py
@@ -1,22 +1,76 @@
 import main
+import const
 
+
+def rockIsPossible(char):
+    if (char == 'k' & const.k):
+        const.k = False
+        return True
+    if (char == 'K' & const.K):
+        const.K = False
+        return True
+
+
+def queenStillStand(char):
+    if (char == 'q' & const.q):
+        const.q = False
+        return True
+    if (char == 'Q' & const.Q):
+        const.Q = False
+        return True
 
 def getPiece(cord):
     return main.a[cord[0]][cord[1]]
 
 
-def is_real(piece, cord1, cord2):
+def is_real(cord1, cord2):
     if (getPiece(cord1) == ' '):
         return False
 
-    if (getPiece(cord1) == 'p'):
-        if (((cord1[2] - cord2[2]) != '1') | (cord1[2] - cord2[2] != '-1')):
-            if (((cord1[1] - cord2[1]) > '2') | (((cord1[1] - cord2) < '1'))):
+    if (getPiece(cord1) == 'p'):  # Pawn rules
+        if (((cord1[1] - cord2[1]) != '1') | (cord1[1] - cord2[1] != '-1')):
+            if (((cord1[0] - cord2[0]) > '2') | (((cord1[0] - cord2[0]) < '1'))):
                 return False
-            if (getPiece(cord2) != ' '):  # Pawn rules
+            if (getPiece(cord2) != ' '):
                 return False
         else:
             if (getPiece(cord2) == ' '):
                 return False
             else:
                 return True
+
+    if (getPiece(cord1) == 'P'):  # Pawn rules
+        if (((cord1[1] - cord2[1]) != '1') | (cord1[1] - cord2[1] != '-1')):
+            if (((cord1[0] - cord2[0]) > '2') | (((cord1[0] - cord2[0]) < '1'))):
+                return False
+            if (getPiece(cord2) != ' '):
+                return False
+        else:
+            if (getPiece(cord2) == ' '):
+                return False
+            else:
+                return True
+
+    if (getPiece(cord1) == 'k'):  # King rules
+        if getPiece(cord2) == 'q':
+            if (queenStillStand('q') & rockIsPossible('k')):
+                return True
+            else:
+                return False
+        if ((((cord2[0] - cord1[0]) == '1') | (cord2[0] - cord1[0] == '-1') | (cord2[1] - cord1[1]) == '1') | (
+                cord2[1] - cord1[1] == '-1') & getPiece(cord2) == ' '):
+            return True
+        else:
+            return False
+
+    if (getPiece(cord1) == 'K'):  # King rules
+        if getPiece(cord2) == 'Q':
+            if (queenStillStand('Q') & rockIsPossible('K')):
+                return True
+            else:
+                return False
+        if ((((cord2[0] - cord1[0]) == '1') | (cord2[0] - cord1[0] == '-1') | (cord2[1] - cord1[1]) == '1') | (
+                cord2[1] - cord1[1] == '-1') & getPiece(cord2) == ' '):
+            return True
+        else:
+            return False
