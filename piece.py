bdac5a6600b3ed904404ef9a30ea8e99d4e5d3c8
Dialjini <danil_kustov@mail.ru>
2019-04-11 16:41:47 +0300

    Add K***ht rules

--- piece.py
+++ piece.py
@@ -29,6 +29,8 @@ def is_real(cord1, cord2):
         return False
 
     if (getPiece(cord1) == 'p'):  # Pawn rules
+        if ((cord1[1] == '2') & (cord2[1] == '4')):
+            return True
         if (((cord1[1] - cord2[1]) != '1') | (cord1[1] - cord2[1] != '-1')):
             if (((cord1[0] - cord2[0]) > '2') | (((cord1[0] - cord2[0]) < '1'))):
                 return False
@@ -41,6 +43,8 @@ def is_real(cord1, cord2):
                 return True
 
     if (getPiece(cord1) == 'P'):  # Pawn rules
+        if ((cord1[1] == '7') & (cord2[1] == '5')):
+            return True
         if (((cord1[1] - cord2[1]) != '1') | (cord1[1] - cord2[1] != '-1')):
             if (((cord1[0] - cord2[0]) > '2') | (((cord1[0] - cord2[0]) < '1'))):
                 return False
@@ -161,3 +165,13 @@ def is_real(cord1, cord2):
                 if getPiece((cord2[0] - bish0) + (cord2[1] - bish0)) != ' ':
                     return False
         return True
+
+    if (getPiece(cord1) == 'n'):  # K***ht rules
+        if ((cord2[0] - cord1[0] <= '2') & (cord2[1] - cord1[1] <= '2') & (cord2[0] - cord1[0] >= '-2') & (
+                cord2[1] - cord1[1] == '-2') & (getPiece(cord2).isupper | getPiece(cord2) == ' ')):
+            return True
+
+    if (getPiece(cord1) == 'N'):  # K***ht rules
+        if ((cord2[0] - cord1[0] <= '2') & (cord2[1] - cord1[1] <= '2') & (cord2[0] - cord1[0] >= '-2') & (
+                cord2[1] - cord1[1] == '-2') & (getPiece(cord2).isupper | getPiece(cord2) == ' ')):
+            return True
